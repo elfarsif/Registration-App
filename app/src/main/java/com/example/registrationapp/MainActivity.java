c4320c4a9f98e777d2c6e2567a49899433637def
Frank Norris El Farsi <f.n.elfarsi@gmail.com>
2021-12-27 21:47:53 -0500

    Created layout and a few buton

--- app/src/main/java/com/example/registrationapp/MainActivity.java
+++ app/src/main/java/com/example/registrationapp/MainActivity.java
@@ -1,14 +1,163 @@
 package com.example.registrationapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.RadioGroup;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
+
+    private EditText edtTxtName,edtTxtEmail,edtTxtPassword,edtTxtPassRepeat;
+    private Button btnPickImage,btnRegister;
+    private TextView txtWarnName,txtWarnEmail,txtWarnPassword,txtWarnPassRepeat;
+    private Spinner countriesSpinner;
+    private RadioGroup rgGender;
+    private CheckBox agreementCheck;
+    private ConstraintLayout parent;
+
+    
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initViews();
+
+        btnPickImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "Yet to talk about", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+               initRegister();
+            }
+        });
+
+
     }
+
+    private void initRegister(){
+        Log.d(TAG,"initRegister: Started");
+
+        if(validateData()){
+            if(agreementCheck.isChecked()){
+                showSnackBar();
+            }else{
+                Toast.makeText(this, "Agree to Statement", Toast.LENGTH_SHORT).show();
+            }
+        }
+
+    }
+
+    private void showSnackBar(){
+        Log.d(TAG,"showSnackBar: Started");
+        txtWarnName.setVisibility(View.GONE);
+        txtWarnEmail.setVisibility(View.GONE);
+        txtWarnPassword.setVisibility(View.GONE);
+        txtWarnPassRepeat.setVisibility(View.GONE);
+
+        Snackbar.make(parent,"User Registered",Snackbar.LENGTH_INDEFINITE)
+                .setAction("Dismiss", new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+
+                    }
+                })
+                .show();
+
+    }
+
+    private Boolean validateData(){
+        Log.d(TAG,"validateData: Started");
+
+        if(edtTxtName.getText().toString().equals("")){
+            txtWarnName.setVisibility(View.VISIBLE);
+            txtWarnName.setText("Enter your name");
+            return false;
+        }
+
+        if(edtTxtEmail.getText().toString().equals("")){
+            txtWarnEmail.setVisibility(View.VISIBLE);
+            txtWarnEmail.setText("Enter your Email");
+            return false;
+        }
+
+        if(edtTxtPassword.getText().toString().equals("")){
+            txtWarnPassword.setVisibility(View.VISIBLE);
+            txtWarnPassword.setText("Enter your password");
+            return false;
+        }
+
+        if(txtWarnPassRepeat.getText().toString().equals("")){
+            txtWarnPassRepeat.setVisibility(View.VISIBLE);
+            txtWarnPassRepeat.setText("Re-enter your password");
+            return false;
+        }
+
+        return true;
+    }
+
+    private void initViews(){
+        Log.d(TAG,"initViews: Started");
+        edtTxtName=findViewById(R.id.edtTxtName);
+        edtTxtEmail=findViewById(R.id.edtTxtEmail);
+        edtTxtPassword=findViewById(R.id.edtTxtPassword);
+        edtTxtPassRepeat=findViewById(R.id.edtTxtPassRepeat);
+
+        btnPickImage=findViewById(R.id.btnPickImage);
+        btnRegister=findViewById(R.id.btnRegister);
+
+        txtWarnName=findViewById(R.id.txtWarnName);
+        txtWarnEmail=findViewById(R.id.txtWarnEmail);
+        txtWarnPassword=findViewById(R.id.txtWarnPassword);
+        txtWarnPassRepeat=findViewById(R.id.txtWarnPassRepeat);
+
+        countriesSpinner=findViewById(R.id.spinnerCountry);
+        rgGender=findViewById(R.id.rgGender);
+        agreementCheck=findViewById(R.id.agreementCheck);
+        parent=findViewById(R.id.parent);
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
